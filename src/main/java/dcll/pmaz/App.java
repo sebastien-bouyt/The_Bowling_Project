d8ca3fd72292830e0ed2c451d735d5488da184cc
princemzb <mazaba_prince@yahoo.fr>
2016-03-09 14:02:48 +0100

    creation du projet BOOLING

---
+++ src/main/java/dcll/pmaz/App.java
@@ -0,0 +1,13 @@
+package dcll.pmaz;
+
+/**
+ * Hello world!
+ *
+ *b
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
